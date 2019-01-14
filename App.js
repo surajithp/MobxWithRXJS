e886a442a253857fedc71fd6dbecbbaaa4043df7
surajith <surajithreddy@gmail.com>
2019-01-14 13:53:49 +0530

    Rendering multiple values

--- App.js
+++ App.js
@@ -14,75 +14,84 @@ const Rx = require("rxjs/Rx");
 const dimensions = Dimensions.get("window");
 const Buttoncomponent = glamorous.view({
   position: "absolute",
-  bottom: 200,
+  bottom: 0,
   left: 180
 });
 
 const MainviewComponent = glamorous.view({
-  height: dimensions.height - 30,
+  height: dimensions.height - 25,
   width: dimensions.width
 });
 @observer
 export default class App extends Component {
-  @observable maximum_value = null;
-  @observable current_value = null;
-  @observable button_title = "Start";
-  @observable toggle_button = true;
+  @observable maximumValue = null;
+  @observable buttonTitle = "Start";
+  @observable toggleButton = true;
+  @observable currentvalues = [];
   dataObserver = {};
   subscription = null;
 
   componentDidMount = () => {
+    console.log("did mount");
     this.createNewobserver();
   };
+
   createNewobserver = () => {
-    this.dataObserver = Rx.Observable.interval(1000)
-      .concatMap(val =>
-        Rx.Observable.of(Math.floor(Math.random() * 1000)).delay(
-          Math.floor(Math.random() * 4) * 1000
+    this.dataObserver = Rx.Observable.from([56, 84, 321, 421, 621])
+      .concat(
+        Rx.Observable.interval(1000).concatMap(val =>
+          Rx.Observable.of(Math.floor(Math.random() * 1000)).delay(
+            Math.floor(Math.random() * 4) * 1000
+          )
         )
       )
       .scan((acc, curr) => {
-        this.current_value = curr;
+        this.currentvalues.push(curr);
         return acc > curr ? acc : curr;
       }, 0);
   };
   createNewSubscription() {
     this.unsubscribeIfNecessary();
     this.subscription = this.dataObserver.subscribe(
-      val => (this.maximum_value = val)
+      val => (this.maximumValue = val)
     );
   }
 
   unsubscribeIfNecessary() {
-    console.log(this.subscription);
-    console.log(this.subscription.closed);
     if (this.subscription && !this.subscription.closed) {
       this.subscription.unsubscribe();
     }
   }
 
-  @action //Where multiple observables states can modify
-  onPressStart= () => {
+  @action //Where multiple observables values can be modify
+  onPressStart = () => {
     console.log("Button Clicked");
-    this.button_title = this.toggle_button ? "Stop" : "Start";
-    if (!this.toggle_button) {
+    this.buttonTitle = this.toggleButton ? "Stop" : "Start";
+    if (!this.toggleButton) {
       //stop
+      console.log("called unsubscribe");
       this.unsubscribeIfNecessary();
     } else {
       //start
+      console.log("called New subscription");
       this.createNewSubscription();
     }
-    this.toggle_button = !this.toggle_button;
+    this.toggleButton = !this.toggleButton;
   };
 
   render() {
     return (
       <MainviewComponent>
         <Buttoncomponent>
-          <Button onPress={this.onPressStart} title={this.button_title} />
+          <Button onPress={this.onPressStart} title={this.buttonTitle} />
         </Buttoncomponent>
-        <Text>Current Value:{this.current_value}</Text>
-        <Text>Max Value:{this.maximum_value}</Text>
+        <Text>
+          Current Values:
+          {this.currentvalues.map((item, i) => (
+            <Text key={i}>{item},</Text>
+          ))}
+        </Text>
+        <Text>Max Value:{this.maximumValue}</Text>
       </MainviewComponent>
     );
   }
